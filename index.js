810c8b03ae87e0b913df8cef4fbb9573e9814920
Jamie Bowley <jamie@jBook.local>
2016-09-04 11:40:44 +1200

    Fix error in index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post('/webhook', (req, res) => {
             wit.runActions(
               sessionId, // the user's current session
               text, // the user's message
-              sessions[sessionId].context // the user's current session state
+              bot.sessions[sessionId].context // the user's current session state
             ).then((context) => {
               // Our bot did everything it has to do.
               // Now it's waiting for further messages to proceed.
@@ -109,7 +109,7 @@ app.post('/webhook', (req, res) => {
               }
 
               // Updating the user's current session state
-              sessions[sessionId].context = context;
+              bot.sessions[sessionId].context = context;
             })
             .catch((err) => {
               console.error('Oops! Got an error from Wit: ', err.stack || err);
